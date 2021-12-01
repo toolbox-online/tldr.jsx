a415eb4f827916efa93cd96d2d5c3c35a16675ae
holmofy <hff1996723@163.com>
2021-12-01 08:47:59 +0800

    Update Analytics.js

--- src/Analytics.js
+++ src/Analytics.js
@@ -1,3 +1,6 @@
 // Workaround because mixpanel.cjs.js doesn't pass flow-checks
 // import Mixpanel from 'mixpanel-browser/dist/mixpanel.cjs.js'
-// export default Mixpanel
+const Mixpanel = ()=>{
+  return <div>{/* 广告位 */}</div>;
+};
+export default Mixpanel
